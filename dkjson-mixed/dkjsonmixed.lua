4fc0a338ab27eb7c600eb4c9fa1a697dfa08fe3b
suhada <suhada@blueyonder.co.uk>
2012-07-07 20:20:37 +0200

    Update master

--- dkjson-mixed/dkjsonmixed.lua
+++ dkjson-mixed/dkjsonmixed.lua
@@ -1,3 +1,31 @@
+--[==[
+=========================================================
+Suhada's extension to dkjson to allow mixed table indexes
+*Version 1.0
+=========================================================
+Without this extension dkjson encoding/decoding doesn't handle mixed table indexes.
+For example decode(encode({a="x","P"})) is {a="x", ["1"] = "P"} (Note key for "P" is a string rather than a number)
+=========================================================
+Usage:
+  Run as an extension to dkjson by:
+
+  local json = require ("dkjson")
+  require("dkjsonmixed")
+  json.mixedfield = "_number_"  --The setting of this variable is what enables mixed index handling by encode and decode
+  The suggested value for mixedfield is "_number_" but any value that will not be used in any tables will be fine.
+  To disable mixed index handling just set dkjson.mixedfield=nil.
+
+dkjson.encode packs all values with numeric indices into a subtable before turning into json.
+  The key of the subtable is dkjson.mixedfield.
+  With dkjson.mixedfield="_number_":
+  encode({a="x","P"}) is {"a":"x","_number_":{"1":"P"}} which decodes correctly.
+
+dkjson.decode looks for tables named dkjson.mixedfield and unpacks them.
+  With dkjson.mixedfield="_number_":
+  decode({"a":"x","_number_":{"1":"P"}}) = {a="x","P"}
+=========================================================
+]==]
+
 local dkjson = require ("dkjson")
 
 local function deepcopy(t)
@@ -15,13 +43,11 @@ local function deepcopy(t)
 end
 
 local keepencode = dkjson.encode
-local keepdecode = dkjson.decode
-
 local function encode (object, state)
     local newobject = object
     local mixed = dkjson.mixedfield
     if mixed then
-        local function savenumeric(tab)
+        local function packnumeric(tab)
             if type(tab) == "table" then
                 --Seperate numeric indices
                 for key, subtab in pairs(tab) do
@@ -34,30 +60,32 @@ local function encode (object, state)
                         --remove from the table
                         tab[key] = nil
                         --recurse
-                        savenumeric(subtab)
+                        packnumeric(subtab)
                     end
                 end
             end
         end
         newobject = deepcopy(object)
-        savenumeric(newobject)
+        packnumeric(newobject)
     end
     return keepencode(newobject, state)
 end
+dkjson.encode = encode
 
+local keepdecode = dkjson.decode
 local function decode (string, position, null)
     local mixed = dkjson.mixedfield
-    local function loadnumeric(tab)
+    local function unpacknumeric(tab)
         if type(tab) == "table" then
             if tab[mixed] then
                 for key, subtab in pairs(tab[mixed]) do
-                    loadnumeric(subtab)
+                    unpacknumeric(subtab)
                     tab[key+0] = subtab
                 end
                 tab[mixed] = nil
             else
                 for _, subtab in pairs(tab) do
-                    loadnumeric(subtab)
+                    unpacknumeric(subtab)
                 end
             end
         end
@@ -65,11 +93,8 @@ local function decode (string, position, null)
 
     local tab = keepdecode (string, position, null)
     if mixed then
-        loadnumeric(tab)
+        unpacknumeric(tab)
     end
     return tab
 end
-
---redefine encode and decode
-dkjson.encode = encode
 dkjson.decode = decode
